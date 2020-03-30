24d38f5eebf737c496ce92265b2db718c891f6a0
fstqwq <fstqwq@foxmail.com>
2020-03-31 00:40:35 +0800

    If you can't see, you see.

--- semantic.bash
+++ semantic.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath /ulib/java/antlr-4.8-complete.jar:./bin Ornn.Main"
-# cat > program.txt   # save everything in stdin to program.txt
+cat > code.mx
 $CCHK
