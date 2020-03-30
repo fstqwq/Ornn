99dff3493ce53be2d5faa7ccc1682f863a869d58
fstqwq <fstqwq@foxmail.com>
2020-03-31 00:28:29 +0800

    update bash

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d bin @/dev/stdin
+find ./src -name *.java | javac -d bin -classpath "/ulib/java/antlr-4.8-complete.jar" @/dev/stdin
